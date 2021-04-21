6ebce275b3a1097e3258f3c03b50126cb8ed67c1
Dogeon188 <dogcatdog188@gmail.com>
2021-04-21 23:35:02 +0800

    says cleanup

--- cmd/says.js
+++ cmd/says.js
@@ -1,13 +1,13 @@
 const bent = require("bent")
 const {stripIndents} = require("common-tags")
+const config = require("../config.json")
+
+let says, lastUpdated = 0
 
 const parseSays = ctx => {
     const ks = ["says", "author"]
     return Object.fromEntries(ks.map(k => [k, ctx[`gsx$${k}`].$t]))
 }
-
-const config = require("../config.json")
-let says, lastUpdated = 0
 
 const updateSays = async (client, forceUpdate) => {
     if (!forceUpdate && new Date().getTime() - lastUpdated < 600000) return
@@ -15,7 +15,14 @@ const updateSays = async (client, forceUpdate) => {
         `https://spreadsheets.google.com/feeds/list/${config.sheetSrc.sheetId}/${config.sheetSrc.says}/public/values?alt=json`,
         "json")()).feed.entry.map(parseSays)
     lastUpdated = new Date().getTime()
-    client.logger.log("info", "Updated ctlo says entries!")
+    client.logger.log("info", `Updated ctlo says entries! (Now have ${says.length} entries.)`)
+}
+
+const greet = msg => {
+    let t = new Date().getHours()
+    if (t >= 18 || t < 4) msg.channel.send("同學們晚安。")
+    else if (t >= 11 && t < 18) msg.channel.send("同學們午安。")
+    else msg.channel.send("同學們早。")
 }
 
 module.exports = {
@@ -27,38 +34,30 @@ module.exports = {
     arg: true,
     usage: `${config.prefix} says (<speech_id>)`,
     execute(client, msg, args) {
-        if (msg.author.id === config.dogeon.id && args[0] === "update") {
-            updateSays(client, true).then(() =>
-            msg.channel.send(`已更新昶語錄！（目前共有 **${says.length}** 個條目）`))
-            return
-        }
-        if (args[0] === "greet") {
-            let t = new Date().getHours()
-            if (t >= 18 || t < 4) msg.channel.send("同學們晚安。")
-            else if (t >= 11 && t < 18) msg.channel.send("同學們午安。")
-            else msg.channel.send("同學們早。")
-            return
-        }
+        if (msg.author.id === config.dogeon.id && args[0] === "update")
+            return updateSays(client, true).then(() =>
+                msg.channel.send(`已更新昶語錄！（目前共有 **${says.length}** 個條目）`))
+
+        if (args[0] === "greet") return greet(msg)
+
         updateSays(client, false).then(() => {
-            const cnt = says[(() => {
+            const s = says[(() => {
                 if (args.length > 0) {
                     let i = +args[0]
                     if (isNaN(i)) throw new Error(`Invalid speech id ${args[0]}!`)
                     if (i >= says.length) throw new Error(`Index ${i} is too big for size ${says.length}!`)
+                    if (-says.length <= i < 0) return says.length + i
                     return i
                 }
                 return Math.floor(Math.random() * says.length)
-            })()].format({
+            })()]
+            msg.channel.send(stripIndents`
+            \`\`\`${s.says.format({
                 username: msg.member.nickname == null ? msg.author.username : msg.member.nickname
-            })
-            msg.channel.send(stripIndents`
-            \`\`\`${cnt}
-            ——昶昶，2021\`\`\``)
+            })}
+            ——${s.author}，2021\`\`\``)
         }).catch(e => {
-            msg.channel.send(stripIndents`
-                ERROR
-                \`${e.stack}\`
-            `)
+            msg.channel.send(`ERROR: \`${e.message}\``)
             throw e
         })
     }
