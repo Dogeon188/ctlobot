b081b6ac828117a705091aa8cd9fa9174a9eedff
Dogeon188 <dogcatdog188@gmail.com>
2021-05-02 10:40:23 +0800

    removed auto update; completed manual update

--- special/update.js
+++ special/update.js
@@ -2,7 +2,8 @@ const {exec} = require("child_process")
 
 module.exports = {
     execute (client, msg, args) {
-        console.log("Trying to update ctlobot!")
+        msg.channel.send("嘗試更新昶昶機器人……")
+        client.logger.log("info", "Trying to update ctlobot!")
         exec("npm run update").stdout.pipe(process.stdout)
     }
 }
--- updater.js
+++
@@ -1,6 +0,0 @@
-const {exec} = require("child_process")
-
-setInterval(() => {
-    console.log("Auto-updating ctlobot! (once every day)")
-    exec("npm run update").stdout.pipe(process.stdout)
-}, require("./config.json").updateInterval)
