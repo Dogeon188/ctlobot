6dc18f7bf4d7f08df3e2e2ff273c751861082210
Dogeon188 <dogcatdog188@gmail.com>
2022-02-27 11:24:05 +0800

    ctlo cls

--- cmd/cls.js
+++ cmd/cls.js
@@ -3,19 +3,26 @@ const { DiscordAPIError } = require("discord.js")
 const client = require("../client")
 
 module.exports = {
-	name: "chat",
+	name: "cls",
 	op: true,
 	async execute(msg, args) {
 		try {
-			if (args.length < 1) throw new InvalidInputError("Didn't provide channel ID!")
-			const channel = (args[0] === ".") ?
+			const channel = (args.length < 1) ?
 				msg.channel :
 				(/<#.+>/.test(args[0])) ?
 					await client.channels.fetch(args[0].match(/(?<=<#).+(?=>)/)[0]) :
 					await client.channels.fetch(args[0])
 			if (!channel.isText())
 				throw new InvalidInputError(`Provided channel ID \`${args[0]}\` is not a text channel!`)
-			channel.send(args[1])
+			channel.messages.fetch({limit: 100}).then(async msgs => {
+				let deleted = msgs.filter(
+					m => m.author.id === client.user.id ||
+						m.content.startsWith(process.env.PREFIX + " "))
+				await channel.bulkDelete(deleted)
+				msg.channel.send(
+					`Cleared **${deleted.size}** messages from **${client.user.username}**.`
+				).then(m => setTimeout(() => m.delete(), 1000))
+			})
 		} catch (e) {
 			if (e instanceof InvalidInputError) msg.channel.send(e.message)
 			if (e instanceof DiscordAPIError) msg.channel.send(`Invalid channel id \`${args[0]}\``)
